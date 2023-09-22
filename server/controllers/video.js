a1ddc0e2e1c8f1662c2c2ddb8c826e5c1c57f290
Dhruv Khandelwal <khandelwaldhruv123@gmail.com>
2023-09-22 18:08:26 +0530

    Upload done

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,25 +1,27 @@
 import videoFiles from '../models/videoFiles.js'
-export const uploadVideo=async(req,res,next)=>{
-    console.log(uploadVideo);
-    if(req.file===undefined){
-        res.status(404).json({message:"Plz Upload a '.mp4' video file only "})
+export const uploadVideo = async (req, res, next) => {
+    console.log(req.body);
+    console.log(req.file);
+  
+    if (req.file === undefined) {
+      res.status(404).json({ message: "Plz Upload a '.mp4' video file only " });
+    } else {
+      try {
+        const file = new videoFiles({
+          videoTitle: req.body.title,
+          fileName: req.file.originalname,
+          filePath: req.file.path,
+          fileType: req.file.mimetype,
+          fileSize: req.file.size,
+          videoChanel: req.body.chanel,
+          Uploader: req.body.Uploader,
+        });
+        await file.save();
+        res.status(200).send("File upload successfully");
+        console.log("Video saved");
+      } catch (error) {
+        console.log(error);
+        res.status(400).send(error.message);
+      }
     }
-    else{
-        try {
-            const file=new videoFiles({
-                videoTitle: req.body.title,
-                fileName: req.file.originalname,
-                filePath: req.file.path,
-                fileType:req.file.mimetype,
-                fileSize: req.file.size,
-                videoChanel: req.body.chanel,
-                Uploader: req.body.Uploader,
-            })
-            await file.save();
-            res.status(201).send("File upload successfully")
-        
-        } catch (error) {
-            res.status(400).send(error.message)
-        }
-    }
-}
+  };
